7f1121036dab026388a0c1a24af3acf667ba5d15
Michele Bonacina <michele.bonacina@gmail.com>
2020-03-23 20:53:03 +0100

    started national stats visualization

--- view/nation.php
+++ view/nation.php
@@ -7,9 +7,9 @@ $globalData = GlobalDataController::loadGlobalData();
 <!-- main -->
 <main>
     <div class="container-fluid">
-        <h1 class="mt-4">Dashboard</h1>
+        <h1 class="mt-4">Situazione Nazionale</h1>
         <ol class="breadcrumb mb-4">
-            <li class="breadcrumb-item active">Dashboard</li>
+            <li class="breadcrumb-item active">Andamento Nazionale al <?= date('d-m-Y H:i', $globalData->date) ?></li>
         </ol>
         <div class="row">
             <div class="col-xl-3 col-md-6">
