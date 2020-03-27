a75c425bdb6c0c592a87b93055fc21da8827a531
Michele Bonacina <michele.bonacina@gmail.com>
2020-03-27 09:51:42 +0100

    removed property data type for compatibility with old php version

--- model/global_data.php
+++ model/global_data.php
@@ -5,18 +5,18 @@
  */
 class GlobalData {
 
-    public int $date;
-    public string $country;
-    public int $synptomaticRecoverd;
-    public int $intensiveCare;
-    public int $totalHospitalized;
-    public int $homeIsolation;
-    public int $totalActualPositive;
-    public int $newActualPositive;
-    public int $dischargedHealed;
-    public int $deceased;
-    public int $totalCases;
-    public int $swabs;
+    public $date;
+    public $country;
+    public $synptomaticRecoverd;
+    public $intensiveCare;
+    public $totalHospitalized;
+    public $homeIsolation;
+    public $totalActualPositive;
+    public $newActualPositive;
+    public $dischargedHealed;
+    public $deceased;
+    public $totalCases;
+    public $swabs;
 
     public function isSameDay(int $date) {
         return date('d-m-Y', $date) == date('d-m-Y', $this->date);
